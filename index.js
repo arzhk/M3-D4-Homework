eade89e525dbcf1a89a1926ab3fe017d974cf3b7
arzhk <72197750+arzhk@users.noreply.github.com>
2020-11-05 14:10:39 +0000

    Added functionality
    - Updated styling
    - Added "add to cart" buttons

--- index.js
+++ index.js
@@ -10,6 +10,35 @@ async function getData() {
   ).then(async (response) => {
     let data = await response.json();
     bookDB.push(data);
-    console.log(bookDB);
+    console.log("Data loaded");
+  });
+  populateStore();
+}
+
+function createStoreCard(book) {
+  const storeBody = document.querySelector("#books-row");
+
+  const newBook = document.createElement("div");
+  newBook.style.maxWidth = "600px";
+  newBook.style.margin = "5px";
+  newBook.innerHTML = `<div class="card book-card mb-3" style="max-width: 18rem;">
+  <div class="card-header">
+  <h4>${book.title}</h4>
+  <p class="mb-0">${book.category}</p>
+  </div>
+  <div class="card-body">
+  <img src="${book.img}" class="w-100" />
+
+    <button class="btn-addtocart my-4" onclick="addToCart()"><i class="fas fa-cart-plus mr-2"></i>Add to Cart</button>
+  </div>
+</div>`;
+  storeBody.appendChild(newBook);
+}
+
+function populateStore() {
+  bookDB[0].forEach((e) => {
+    createStoreCard(e);
   });
 }
+
+window.onload = getData();
